4db5abd9cfe328e01a8367b2a933f6d1c0fafee0
Xi LIU <xi.liu@u.nus.edu>
2024-02-06 21:53:23 +0800

    construct h

--- dbExchangeModel.cpp
+++ dbExchangeModel.cpp
@@ -2,16 +2,117 @@
 // Created by polya on 2/6/24.
 //
 #include "dbExchangeModel.hpp"
-//
-//mat20c dbExchangeModel::constructhPart(){
-//
-//// Now it is a dense matrix since the dimension is not large
-//
-//mat20c midMat;
-//for(int j=1;j<L;j++){
-//    midMat(j-1,j)=1;
-//    midMat(j,j-1)=1;
-//    midMat*=-t;
-//}
-//
-//}
+
+
+///
+/// @param LHS electron part of the matrix
+/// @param RHS spin part of the matrix
+/// @return Kronecker product of the 2 matrices
+mat20c dbExchangeModel::kron(const mat10c & LHS, const mat2c & RHS) {
+
+    mat20c retMat=mat20c::Zero();
+    for (int i=0;i<LHS.rows();i++){
+        for (int j=0;j<LHS.cols();j++){
+            retMat.block<2,2>(i*RHS.rows(),j*RHS.rows())=LHS(i,j)*RHS;
+        }
+    }
+
+    return retMat;
+
+}
+
+
+void dbExchangeModel::constructhPart(){
+
+    for(int j=0;j<this->M;j++){
+        double K=this->KSupValsAll[j];
+        mat20c retMat=mat20c::Zero();
+
+        mat10c tmp0=mat10c::Zero();
+        tmp0(this->L-1,0)=-t*std::exp(L*K*1i);
+
+        retMat+=this->kron(tmp0,I2);
+
+        mat10c tmp1=mat10c::Zero();
+        tmp1(0,this->L-1)=-t*std::exp(-L*K*1i);
+
+        retMat+=this->kron(tmp1,I2);
+
+        for(int l=1;l<=this->L-1;l++){
+            mat10c tmp=mat10c::Zero();
+            tmp(l-1,l)=-t;
+            tmp(l,l-1)=-t;
+            retMat+=this->kron(tmp,I2);
+        }
+
+        this->preComputedHamiltonianPart.push_back(retMat);
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
+mat20c dbExchangeModel::hEig(std::vector<double> &s, const int &j) {
+    mat20c part=this->preComputedHamiltonianPart[j];
+    double sSum=0;
+    for(int l=0;l<L;l++){
+        sSum+=s[l]*s[(l+1)%L];
+
+    }
+
+    part+=this->I20*sSum;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dbExchangeModel.hpp
+++ dbExchangeModel.hpp
@@ -8,58 +8,69 @@
 #include <Eigen/Dense>
 #include <unsupported/Eigen/MatrixFunctions>
 #include <Eigen/Eigenvalues>
+#include <numbers>
+#include<complex>
+using namespace std::complex_literals;
+using mat10c = Eigen::Matrix<std::complex<double>, 10, 10>;
+using mat2c = Eigen::Matrix<std::complex<double>, 2, 2>;
+using mat20c = Eigen::Matrix<std::complex<double>, 20, 20>;
+const auto PI=std::numbers::pi;
 
-
-using mat10c= Eigen::Matrix< std::complex<double>, 10, 10 >;
-using mat2c=Eigen::Matrix< std::complex<double>, 2, 2 >;
-using mat20c=Eigen::Matrix< std::complex<double>, 20, 20 >;
-class dbExchangeModel{
+class dbExchangeModel {
 public:
-    dbExchangeModel(double temperature){
-        this->T=temperature;
+    dbExchangeModel(double temperature) {
+        this->T = temperature;
+
+        //construct SBZ values
+        for (int j = 0; j < this->M; j++) {
+            this->KSupValsAll.push_back(2 * PI * double(j) / (double(this->L * this->M)));
+        }
+
+        this->constructhPart();
+
+        this->I10upup= this->kron(mat10c::Identity(),upup)*this->g;
+        this->I10downdown=this->kron(mat10c::Identity(),downdown)*this->g;
+
 
     }
 
 
 public:
-    int part=0; // a group of computations
-    int L=0;// length of a supercell
-    int M=20;// number of supercells
+    int part = 0; // a group of computations
+    int L = 10;// length of a supercell
+    int M = 20;// number of supercells
 
-    double Ne=static_cast<double>(M);// electron number
+    double Ne = static_cast<double>(M);// electron number
 
-    double t=0.4;// hopping coefficient
-    double J=-1;// exchange interaction
+    double t = 0.4;// hopping coefficient
+    double J = -1;// exchange interaction
 
-    double g=0;// coupling coefficient
+    double g = 0;// coupling coefficient
 
-    double T=0;// temperature
+    double T = 1;// temperature
+    std::vector<double> KSupValsAll;//all the values in SBZ
+    std::vector<mat20c> preComputedHamiltonianPart;//part of the SBZ Hamiltonian that can be precomputed
+    mat20c I10upup; // electron spin interaction part
+    mat20c I10downdown;// electron spin interaction part
+
+
+
 
 //    using mat10c= Eigen::Matrix< std::complex<double>, 10, 10 >;
 //    using mat2c=Eigen::Matrix< std::complex<double>, 2, 2 >;
-    mat2c I2{{1,0},{0,1}};
+    mat2c I2{{1, 0},
+             {0, 1}};
+    mat2c upup{{1,0},{0,0}};
+    mat2c downdown{{0,0},{0,1}};
+    mat20c I20=mat20c::Identity()*J;
 //    using mat20c=Eigen::Matrix< std::complex<double>, 20, 20 >;
-
-    mat20c constructhPart();// construct one part of Hamltonian
-    mat20c  hEig(const int &j, std::vector<float>);
-
-
-
-
-
-
-
-
-
-
-
-
+public:
+    mat20c kron(const mat10c &, const mat2c &);// perform Kronecker product
+    void constructhPart();// construct the precomputable part of Hamiltonian
+    mat20c hEig(std::vector<double> &s, const int &j); //using s value from each MC step to construct SBZ Hamiltonian, and compute its eigenvalue problem
 
 
 };
-
-
-
 
 
 #endif //PARAMAGNET_INTERACTION_CPP_DBEXCHANGEMODEL_HPP
