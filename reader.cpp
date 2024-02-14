0d359a128858bc0044fe9ed497db87b1f3c770d1
Xi LIU <xi.liu@u.nus.edu>
2024-02-14 16:46:14 +0800

    segmentation fault fixed

--- reader.cpp
+++ reader.cpp
@@ -7,23 +7,23 @@ int main(int argc, char **argv){
     loader.filesAllSorted();
     loader.fillIntodataStorage();
 
-//    double mu=loader.storages[1].muAll[7];
-//    double E=loader.storages[9].EAll[1];
-//std::cout<<loader.storages[1].EAll[1]<<std::endl;
-//    std::vector<double> s=loader.storages[8].sAll[5];
-//    std::tuple<int,std::vector<double>,std::vector<std::complex<double>> > onetp=loader.storages[4].flattenedEigSolution[3][7];
-//
-//    std::cout<<"mu="<<mu<<std::endl;
-////    std::cout<<"E="<<E<<std::endl;
-//
-//    std::cout<<"==================="<<std::endl;
-//    std::cout<<"s: ";
-//    for(auto val: s){
-//        std::cout<<val<<", ";
-//    }
-//    std::cout<<std::endl;
-//
-//    std::cout<<"eig="<<std::get<1>(onetp)[9]<<std::endl;
+    double mu=loader.storages[1].muAll[7];
+    double E=loader.storages[2].EAll[1];
+
+    std::vector<double> s=loader.storages[0].sAll[5];
+    std::tuple<int,std::vector<double>,std::vector<std::complex<double>> > onetp=loader.storages[1].flattenedEigSolution[3][7];
+
+    std::cout<<"mu="<<mu<<std::endl;
+    std::cout<<"E="<<E<<std::endl;
+
+    std::cout<<"==================="<<std::endl;
+    std::cout<<"s: ";
+    for(auto val: s){
+        std::cout<<val<<", ";
+    }
+    std::cout<<std::endl;
+
+    std::cout<<"eig="<<std::get<1>(onetp)[9]<<std::endl;
 
 
 std::cout<<"end"<<std::endl;
