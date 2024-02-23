8c441790884cb1b487ec16fdc2cdac5b40491f7e
Xi LIU <xi.liu@u.nus.edu>
2024-02-23 16:37:26 +0800

    names

--- generateFiles.py
+++ generateFiles.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #this script generates MCMC computing scripts
 
-TAll=np.linspace(0.1,3.1,70)
+TAll=np.linspace(0.05,3.1,100)
 
 counter=0
 for T in TAll:
@@ -12,13 +12,13 @@ for T in TAll:
     bashContents.append("#SBATCH -N 1\n")
     bashContents.append("#SBATCH -t 0-40:00\n")
     bashContents.append("#SBATCH -p CLUSTER\n")
-    bashContents.append("#SBATCH --mem=140GB\n")
+    bashContents.append("#SBATCH --mem=80GB\n")
     bashContents.append("#SBATCH -o outmc" + str(counter) + ".out\n")
     bashContents.append("#SBATCH -e outmc" + str(counter) + ".err\n")
     bashContents.append("cd /home/cywanag/liuxi/Documents/cppCode/paramagnet_interaction_cpp\n")
     command="./mc "+str(T)
     bashContents.append(command)
-    bsFileName="./mcBash/mcsh"+str(counter)+".sh"
+    bsFileName="./mcBash/mc"+str(counter)+".sh"
     fptrTmp=open(bsFileName,"w+")
     for oneline in bashContents:
         fptrTmp.write(oneline)
